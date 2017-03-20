d9830c0ac6066e0715faddb1cfb47ab4b935a0fa
matt2405warner <mattwarner@rogers.com>
2017-03-20 10:06:15 -0400

    Final initial commit

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,1 +1,10 @@
-This will soon have route data/
+var express = require('express');
+var ctrlSample = require('../controllers/sample.controllers.js');
+var router = express.Router();
+
+router
+  .route('/json')
+  .get(ctrlSample.sampleGetAll);
+
+
+module.exports = router;
